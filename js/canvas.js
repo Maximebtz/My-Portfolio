375f477a55c788e5eca34f84cecaedb81c26dabc
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-12-06 13:03:46 +0100

    New 3D Portfolio

---
+++ js/canvas.js
@@ -0,0 +1,9 @@
+import { Application } from "../runtime.js";
+
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/W4JH15Ek-Kqb6Xlq/scene.splinecode');
+
+
+
